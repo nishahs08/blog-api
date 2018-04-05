085f4bd7aacea900b67bc5b84ab7cf17652659ab
nishahs08 <nishaakashona@live.com>
2018-04-05 17:03:32 +0530

    modified db connection.

--- app.js
+++ app.js
@@ -6,5 +6,4 @@ require('./config/express')(app);
 require('./config/db')()
     .then(() => {
         app.listen(3000, () => console.log('Listening on port 3000'));
-    })
-    .catch(console.log);
+    });
--- config/db.js
+++ config/db.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
 
-module.exports = function () {
-    return new Promise(function (resolve, reject) {
-        mongoose.connect('mongodb://localhost/blogDb', (err)=>  {
-            if(err)
-                reject(err);
-            else {
-                console.log("mongodb connection established");
-                resolve();
-            }
-        });
+module.exports = () => new Promise((resolve, reject) => {
+    mongoose.connect('mongodb://localhost/blogDb');
+
+    mongoose.connection.on('connected',function(){
+            console.log('connected');
+            resolve('connected');
     });
-};
+
+    mongoose.connection.on('error',function(err){
+        console.log('not connected',err)
+        reject(err);       
+    });
+
+    mongoose.connection.on('disconnected',function(){
+        console.log('disconnected');
+    });
+
+    process.on('SIGNIT',function(){
+        mongoose.connection.close(function(){
+            console.log('closed connection');
+            process.exit(0);
+        })
+    })
+});
